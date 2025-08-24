61b9eae00994617f3a302af871ef802c83f60d33
pavithra <pavithraganapathy04@gmail.com>
2025-08-24 16:48:16 +0530

    Use Atlas Mongo URI

--- db.py
+++ db.py
@@ -9,9 +9,11 @@ load_dotenv(dotenv_path=env_path)
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB", "pathfinder")
 
-client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+# Connect with SSL/TLS support
+client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=10000)
 db = client[MONGO_DB]
 
+# Test connection
 try:
     client.admin.command('ping')
     print("MongoDB connected successfully!")
